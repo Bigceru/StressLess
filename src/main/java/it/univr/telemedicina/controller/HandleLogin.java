42bf057603700c312dea29e98ca316003e8ed247
Bigceru <77098588+Bigceru@users.noreply.github.com>
2023-05-16 21:19:12 +0200

    Delete src/main directory

--- src/main/java/it/univr/telemedicina/controller/HandleLogin.java
+++
@@ -1,4 +0,0 @@
-package it.univr.telemedicina.controller;
-
-public class HandleLogin {
-}
--- src/main/java/it/univr/telemedicina/exceptions/NameErrorException.java
+++
@@ -1,7 +0,0 @@
-package it.univr.telemedicina.exceptions;
-
-public class NameErrorException extends RuntimeException{
-    public NameErrorException() {
-        super("Invalid username!");
-    }
-}
