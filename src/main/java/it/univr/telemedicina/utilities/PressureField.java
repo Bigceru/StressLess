1588acf3780d8f563216e3c682da1474c872e763
Bigceru <davide.ceru02@gmail.com>
2023-07-03 23:59:37 +0200

    Upload 3.7 - Created Analyze table for pressure and therapy from DoctorStatisticsSceneController, added button in Pressure.java to display it in analyze table. TODO: check email, and if wanted large the columns in therapy graph.

---
+++ src/main/java/it/univr/telemedicina/utilities/PressureField.java
@@ -0,0 +1,11 @@
+package it.univr.telemedicina.utilities;
+
+public enum PressureField {
+    ID_PATIENT,
+    DATE,
+    HOUR,
+    SYSTOLIC_PRESSURE,
+    DIASTOLIC_PRESSURE,
+    SYMPTOMS,
+    CONDITION_PRESSURE
+}
